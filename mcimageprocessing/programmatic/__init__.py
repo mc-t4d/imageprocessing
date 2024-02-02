3bcf6ed8bafad6613a8b248742733e074f915915
dowhn <ndowhaniuk@gmail.com>
2024-02-02 18:31:21 +0300

    Refactor imports and update `__all__` directive
    The import statement in `mcimageprocessing/programmatic/__init__.py` has changed to import all utilities from `shared_functions`. In addition, the `__all__` directive was updated to list all importable elements to support `import *` usage. This enhances modularity, ease of access, and organization while maintaining a clear track of available functions and classes.

--- mcimageprocessing/programmatic/__init__.py
+++ mcimageprocessing/programmatic/__init__.py
@@ -5,6 +5,14 @@ from .APIs.GloFasAPI import GloFasAPI, GloFasAPINotebookInterface
 from .APIs.GPWv4 import GPWv4, GPWv4NotebookInterface
 from .APIs.ModisNRT import ModisNRT, ModisNRTNotebookInterface
 from .APIs.WorldPop import WorldPop, WorldPopNotebookInterface
-from .shared_functions import *  # Assuming you want to expose everything from shared_functions
+from .shared_functions.utilities import *
 
-__all__ = ['EarthEngineManager', 'GloFasAPI', 'GPWv4', 'ModisNRT', 'WorldPop', 'shared_functions', 'EarthEngineNotebookInterface', 'GloFasAPINotebookInterface', 'GPWv4NotebookInterface', 'ModisNRTNotebookInterface', 'WorldPopNotebookInterface']
+# Add all functions and classes to __all__ for import * support
+__all__ = [
+    'EarthEngineManager', 'GloFasAPI', 'GPWv4', 'ModisNRT', 'WorldPop',
+    'EarthEngineNotebookInterface', 'GloFasAPINotebookInterface', 'GPWv4NotebookInterface',
+    'ModisNRTNotebookInterface', 'WorldPopNotebookInterface',
+    # Add all functions and classes from utilities to __all__
+    'mosaic_images', 'process_and_clip_raster', 'get_raster_min_max', 'add_clipped_raster_to_map',
+    'inspect_grib_file', 'clip_raster'
+]
