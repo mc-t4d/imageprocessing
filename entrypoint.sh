aec502fc93eeb1cd2a524a5074c812702cdf51e8
dowhn <ndowhaniuk@gmail.com>
2024-04-12 15:10:14 +0200

    Updates to ModisNRT file, as well as creation of docker-build.yml github actions file, that now handles automatic builds and github secrets -> docker secrets.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 # Check if the decryption key is provided
-if [ -z "$DECRYPTION_KEY" ]; then
-  echo "DECRYPTION_KEY is not set"
-  exit 1
-fi
+if [ -n "$DECRYPTION_KEY" ]; then
+    # Decrypt the configuration file
+    openssl enc -aes-256-cbc -d -in /usr/src/app/mcimageprocessing/config/config.enc -out /usr/src/app/mcimageprocessing/config/config.yaml -k "$DECRYPTION_KEY"
 
-# Decrypt the configuration file
-openssl enc -aes-256-cbc -d -in /usr/src/app/mcimageprocessing/config/config.enc -out /usr/src/app/mcimageprocessing/config/config.yaml -k "$DECRYPTION_KEY"
-
-# Check if decryption was successful
-if [ $? -ne 0 ]; then
-  echo "Decryption failed"
-  exit 1
+    # Check if decryption was successful
+    if [ $? -ne 0 ]; then
+        echo "Decryption failed"
+        exit 1
+    fi
+else
+    echo "DECRYPTION_KEY is not set, skipping decryption."
 fi
 
 # Execute the main command
 exec "$@"
+
+
+## Check if the decryption key is provided
+#if [ -z "$DECRYPTION_KEY" ]; then
+#  echo "DECRYPTION_KEY is not set"
+#  exit 1
+#fi
+#
+## Decrypt the configuration file
+#openssl enc -aes-256-cbc -d -in /usr/src/app/mcimageprocessing/config/config.enc -out /usr/src/app/mcimageprocessing/config/config.yaml -k "$DECRYPTION_KEY"
+#
+## Check if decryption was successful
+#if [ $? -ne 0 ]; then
+#  echo "Decryption failed"
+#  exit 1
+#fi
+#
+## Execute the main command
+#exec "$@"
 
